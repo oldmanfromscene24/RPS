ad197048004af625e9dd587dbf6e4f82e2717fbc
oldmanfromscene24 <giancarlo.spiller@gmail.com>
2024-03-11 22:37:02 +0100

    feat(html, js): add event listener, DOM modifiers
    Remade with event listeners and DOM modifiers for The Odin Project:
    Revisiting Rock Paper Scissors

--- javascript.js
+++ javascript.js
@@ -1,68 +1,105 @@
-function getComputerChoice () {
-    const computerChoice = ["rock", "paper", "scissors"];
-    return computerChoice[Math.floor(Math.random() * 3)];
-}
+const div = document.querySelector("div");
+let playerCount = 0;
+let computerCount = 0;
+let tieCount = 0;
+div.addEventListener("click", (e) => {
+  const playerSelection = e.target.className;
+  const computerSelection = getComputerChoice();
+  const newDiv = document.createElement("div");
+  const computerChoiceText =
+    "Computer chooses " + computerSelection.toUpperCase() + ". ";
+  const winner = playRound(playerSelection, computerSelection);
+  let winnerText = "";
+  let resultText = "";
 
+  switch (winner) {
+    case "player":
+      playerCount++;
+      winnerText =
+        playerSelection.toUpperCase() +
+        " beats " +
+        computerSelection.toUpperCase() +
+        ". Player wins!";
+      break;
+    case "computer":
+      computerCount++;
+      winnerText =
+        computerSelection.toUpperCase() +
+        " beats " +
+        playerSelection.toUpperCase() +
+        ". Computer wins!";
+      break;
+    case "tie":
+      winnerText = "It's a tie!";
+      tieCount++;
+  }
 
-function playRound (playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+  newDiv.textContent = computerChoiceText + winnerText;
+  div.appendChild(newDiv);
 
-        switch(playerSelection) { //returns the winner if any
-        case "rock": 
-            if (computerSelection === "paper")
-                return "computer";
-            else if (computerSelection === "scissors")
-                return "player";
-            break;
-        case "paper":
-            if (computerSelection === "rock")
-                return "player";
-            else if (computerSelection === "scissors")
-                return "computer";
-            break;
-        case "scissors":
-            if (computerSelection === "rock")
-                return "computer";
-            else if (computerSelection === "paper")
-                return "player";
-            break;             
-    }
-
-    return "tie";
-}
-
-
-function playGame () {
-    let playerCount = 0;
-    let computerCount = 0;
-    let tieCount = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, paper or Scissors?");
-        const computerSelection = getComputerChoice();
-        const playerWin = "Computer chooses " + computerSelection.toUpperCase() + ".\n" + playerSelection.toUpperCase() + " beats " + computerSelection.toUpperCase() + ". Player wins!";
-        const computerWin = "Computer chooses " + computerSelection.toUpperCase() + ".\n" + computerSelection.toUpperCase() + " beats "  + playerSelection.toUpperCase() + ". Computer wins!";
-        const tieWin = "Computer chooses " + computerSelection.toUpperCase() + ".\n" + "It's a tie!"
-        const result = playRound(playerSelection, computerSelection);
-    
-        switch(result) {
-            case "player":
-                playerCount++;
-                alert(playerWin);
-                break;
-            case "computer":
-                computerCount++;
-                alert(computerWin);
-                break;
-            case "tie":
-                tieCount++;
-                alert(tieWin);
-        }
-    }
-
+  if (playerCount + computerCount + tieCount === 5) {
     if (playerCount > computerCount)
-        alert("Player wins " + playerCount + "-" + computerCount + " , " + tieCount + " tie(s)!");
+      resultText =
+        "Player wins " +
+        playerCount +
+        "-" +
+        computerCount +
+        ", " +
+        tieCount +
+        " tie(s)!";
     else if (playerCount < computerCount)
-        alert("Computer wins " + computerCount + "-" + playerCount + " , " + tieCount + " tie(s)!");
-    else alert("Global tie! " + tieCount + " tie(s) , " + playerCount + " wins each!")
+      resultText =
+        "Computer wins " +
+        computerCount +
+        "-" +
+        playerCount +
+        ", " +
+        tieCount +
+        " tie(s)!";
+    else
+      resultText =
+        "Global tie! " + tieCount + " tie(s), " + playerCount + " wins each!";
+
+    const winDiv = document.createElement("div");
+    winDiv.textContent = resultText;
+    div.appendChild(winDiv);
+
+    //starts new match
+    playerCount = 0;
+    computerCount = 0;
+    tieCount = 0;
+
+    const line = document.createElement("div");
+    line.textContent = "------------------";
+    div.appendChild(line);
+
+    const veryNewDiv = document.createElement("div");
+    veryNewDiv.textContent = "NEW MATCH!";
+    div.appendChild(veryNewDiv);
+  }
+});
+
+function getComputerChoice() {
+  const computerChoice = ["rock", "paper", "scissors"];
+  return computerChoice[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  switch (
+    playerSelection //returns the winner if any
+  ) {
+    case "rock":
+      if (computerSelection === "paper") return "computer";
+      else if (computerSelection === "scissors") return "player";
+      break;
+    case "paper":
+      if (computerSelection === "rock") return "player";
+      else if (computerSelection === "scissors") return "computer";
+      break;
+    case "scissors":
+      if (computerSelection === "rock") return "computer";
+      else if (computerSelection === "paper") return "player";
+      break;
+  }
+  return "tie";
 }
